5df251577ec55131f0887efe0ccf149e34a5f4c9
Lubna Abidah <lubna16abidah@gmail.com>
2022-06-01 07:34:50 +0700

    edit index.js

--- index.js
+++ index.js
@@ -27,16 +27,14 @@ const listBelanjaan = belanjaan => {
     return belanjaan.map(belanja =>`- ${belanja.nama} x ${belanja.kuantitas}`)
 };
 
+const hargaBelanjaan = total => {
+    return total.map(total => total.harga * total.kuantitas)
+};
+
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
 //const totalBelanjaan = null;
-const hargaBelanjaan = function(harga, kuantitas){
-  return harga * kuantitas;
-}
 
-const hargaBelanjaan1 = hargaBelanjaan(myObj[0].harga,myObj[0].kuantitas);
-const hargaBelanjaan2 = hargaBelanjaan(myObj[1].harga,myObj[1].kuantitas);
-const hargaBelanjaan3 = hargaBelanjaan(myObj[2].harga,myObj[2].kuantitas);
 
 let totalBelanjaan = () => {
     return hargaBelanjaan1 + hargaBelanjaan2 + hargaBelanjaan3;
